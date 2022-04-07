5d67afc25249895fd9934524dea2c35cd7ae78aa
ThatEpicBanana <thatepicbanana132@gmail.com>
2022-04-07 01:18:17 -0400

    started the parser

---
+++ src/part/attribute.rs
@@ -0,0 +1,32 @@
+use crate::prelude::*;
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct Attribute {
+    pub inner: bool,
+    pub path: path::Path,
+    pub value: Vec<Token>,
+}
+
+fn doc_comment(inn: bool) -> impl Parser<Token, Attribute, Error = Simple<Token>> {
+    filter(move |tok: &Token| 
+        matches!(tok, 
+            DOC_COMMENT { com: _, inner } 
+                if inner == &inn.clone()
+        )
+    ).map(move |tok| 
+        Attribute{ 
+            inner: inn, 
+            path: "doc".into(), 
+            value: vec![tok] 
+        } 
+    )
+}
+
+fn outer_attribute() -> impl Parser<Token, Attribute, Error = Simple<Token>> {
+    just(op(OP_HASH))
+        .ignore_then(
+            path::path().then(
+                just(op(OP_EQUAL))
+            )
+        )
+}
